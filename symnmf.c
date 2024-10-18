e6d10cafb484c6f959fc47a0061f40ad26a65e69
IdoXpoz <ido@hossen.co>
2024-10-18 16:34:10 +0300

    simplify and improve readability of symnmf.c

--- symnmf.c
+++ symnmf.c
@@ -34,6 +34,19 @@ void transpose_matrix(double *mat, double *target, int n, int d) {
     }
 }
 
+/* calc matrix^-0.5 */
+void calc_degree_inv_sqrt_matrix(double *degree_matrix, double *degree_inv_sqrt, int n) {
+    int i;
+    for (i = 0; i < n * n; i++) {
+        double degree_value = degree_matrix[i];
+        if (degree_value != 0.0) {
+            degree_inv_sqrt[i] = 1.0 / sqrt(degree_value);
+        } else {
+            degree_inv_sqrt[i] = 0.0;
+        }
+    }
+}
+
 void copy_matrix(double *mat, double *target, int n, int d) {
     int row, col;
     for (row = 0; row < n; row++) {
@@ -74,80 +87,75 @@ int check_matrix_convergence(double *H, double *new_H, int n, int k) {
 
 }
 
-/* Function to read data points from file */ 
-double* read_points_from_file(char *file_name, int *n, int *d) {
+/* Function to get the dimensions of the data (n and d) from the file */
+void get_dimensions_from_file(char *file_name, int *n, int *d) {
     FILE *file;
-    char line[1024];
-    char *str, *token;
-    int count, row, col;
-    double *data;
+    char line[1024], *str;
+    *n = 0; *d = 0;
     
-    *n = 0;
-    *d = 0;
-    
-    /* Open the file for reading */
     file = fopen(file_name, "r");
     if (!file) {
         fprintf(stderr, "An Error Has Occurred\n");
         exit(1);
     }
     
-    /* First pass: Count the number of rows and columns */
+    /* Count the number of rows and columns */
     while (fgets(line, sizeof(line), file)) {
-        /* If it's the first line, count the number of columns */
-        if (*n == 0) {
-            count = 0;
+        if (*n == 0) { /* If it's the first line, count the number of columns */
             str = line;
             while (*str) {
                 if (*str == ',') {
-                    count++;
+                    (*d)++;
                 }
                 str++;
             }
-            *d = count + 1;  /* Number of columns is one more than the number of commas */
+            (*d)++;  /* Number of columns is one more than the number of commas */
         }
         (*n)++;
     }
     
-    /* Close the file after counting */
     fclose(file);
+}
+
+/* Function to read data points from file */ 
+double* read_points_from_file(char *file_name, int n, int d) {
+    FILE *file;
+    char line[1024], *str, *token;
+    int count, row = 0, col = 0;
+    double *data;
     
     /* Allocate memory for the data array based on n and d */
-    data = (double*)calloc((*n) * (*d), sizeof(double));
+    data = (double*)calloc(n * d, sizeof(double));
     if (data == NULL) {
         fprintf(stderr, "Memory allocation failed\n");
         exit(1);
     }
     
-    /* Second pass: Reopen the file and read the data into the array */
     file = fopen(file_name, "r");
     if (!file) {
         fprintf(stderr, "An Error Has Occurred\n");
         exit(1);
     }
     
-    row = 0;
-    
     while (fgets(line, sizeof(line), file)) {
         /* Split each line by commas and convert to double */
         token = strtok(line, ",");
-        for (col = 0; col < *d; col++) {
+        for (col = 0; col < d; col++) {
             if (token) {
-                data[row * (*d) + col] = atof(token);
+                data[row * d + col] = atof(token);
                 token = strtok(NULL, ",");
             }
         }
         row++;
     }
     
-    /* Close the file after reading */
     fclose(file);
 
     return data;
 }
 
 /* Function to calculate the squared Euclidean distance between two vectors */
-double calc_squared_euclidan_distance_for_two_vectors(double *A, double *B, int d){
+double calc_squared_euclidean_distance_for_two_vectors(double *A, double *B, int d){
     double dist_squared = 0.0;
     for (int k = 0; k < d; k++) {
         double diff = A[k] - B[k];
@@ -216,11 +224,12 @@ void sym(double *data, int n, int d, double *similarity_matrix) {
             if (i == j) {
                 similarity_matrix[i * n + j] = 0.0;
             } else {
-                dist_squared = calc_squared_euclidan_distance_for_two_vectors(&data[i * d], &data[j * d], d);
+                dist_squared = calc_squared_euclidean_distance_for_two_vectors(&data[i * d], &data[j * d], d);
                 similarity_matrix[i * n + j] = exp(-dist_squared / 2.0);
             }
         }
     }
+
     // printf("similarity matrix:\n");
     // print_matrix(similarity_matrix, n, n);
     // printf("exiting sym\n");
@@ -241,7 +250,7 @@ void ddg(double *data, int n, int d, double *degree_matrix) {
 
     sym(data, n, d, similarity_matrix);
     
-    /* Calc degree sum for every row */
+    /* Calc degree sum for every row of the symetric matrix */
     for (i = 0; i < n; i++) {
         degree_sum = 0.0;
         
@@ -262,7 +271,6 @@ void ddg(double *data, int n, int d, double *degree_matrix) {
 
 /* Function to compute normalized similarity matrix*/ 
 void norm(double *data, int n, int d, double *norm_matrix) {
-    // printf("entered norm\n");
     int i;
     double *similarity_matrix = (double*)calloc(n * n, sizeof(double));
     double *degree_matrix = (double*)calloc(n * n, sizeof(double));
@@ -272,79 +280,59 @@ void norm(double *data, int n, int d, double *norm_matrix) {
         fprintf(stderr, "Memory allocation failed\n");
         exit(1);
     }
-
+    /* calc sym and ddg matrices*/
     sym(data, n, d, similarity_matrix);
     ddg(data, n, d, degree_matrix);
-    for (i = 0; i < n * n; i++) {
-        double degree_value = degree_matrix[i];
-        if (degree_value != 0.0) {
-            degree_inv_sqrt[i] = 1.0 / sqrt(degree_value);
-        } else {
-            degree_inv_sqrt[i] = 0.0;
-        }
-    }
-    
+    /* calc degree_inv_sqrt matrix (D^-0.5)*/
+    calc_degree_inv_sqrt_matrix(degree_matrix, degree_inv_sqrt, n);
+    /* Calc (D^-0.5 * A * D^-0.5) in two steps */
     double *result_1 = (double*)calloc(n * n, sizeof(double));
     multiply_matrices(degree_inv_sqrt, similarity_matrix, result_1, n, n, n);
     multiply_matrices(result_1, degree_inv_sqrt, norm_matrix, n, n, n); 
 
-    // printf("normalized matrix:\n");
-    // print_matrix(norm_matrix, n, n);
-    // printf("exiting norm\n");
-
     free(similarity_matrix);
     free(degree_matrix);
+    free(degree_inv_sqrt);
 }
 
 /* Main function*/ 
 int main(int argc, char *argv[]) {
-    char *goal;
-    char *file_name;
-    int n, d;
-    double *data, *similarity_matrix, *degree_matrix, *norm_matrix;
+    char *goal, *file_name;
+    int n = 0, d = 0;
+    double *data, *goal_matrix;
 
     if (argc != 3) {
         fprintf(stderr, "An Error Has Occurred7\n");
         return 1;
     }
-
     goal = argv[1];
     file_name = argv[2];
-    data = read_points_from_file(file_name, &n, &d);
 
-
-    if (strcmp(goal, "sym") == 0) {
-        similarity_matrix = (double *)calloc(n * n, sizeof(double));
-        if (!similarity_matrix) {
-            fprintf(stderr, "An Error Has Occurred8\n");
-            return 1;
-        }
-        sym(data, n, d, similarity_matrix);
-        print_matrix(similarity_matrix, n, n); 
-        free(similarity_matrix);
-    } else if (strcmp(goal, "ddg") == 0) {
-        degree_matrix = (double *)calloc(n * n, sizeof(double));
-        if (!degree_matrix) {
-            fprintf(stderr, "An Error Has Occurred9\n");
-            return 1;
-        }
-        ddg(data, n, d, degree_matrix);
-        print_matrix(degree_matrix, n, n); 
-        free(degree_matrix);
-    } else if (strcmp(goal, "norm") == 0) {
-        norm_matrix = (double *)calloc(n * n, sizeof(double));
-        if (!norm_matrix) {
-            fprintf(stderr, "An Error Has Occurred10\n");
-            return 1;
-        }
-        norm(data, n, d, norm_matrix);
-        print_matrix(norm_matrix, n, n); 
-        free(norm_matrix);
-    } else {
-        fprintf(stderr, "An Error Has Occurred11\n");
+    /* Read the data points from the file, first calculate num of points(n) and dimension(d) */
+    get_dimensions_from_file(file_name, &n, &d); /* updates n and d*/
+    data = read_points_from_file(file_name, n, d);
+    /* initialize our goal_matrix, will be used for similarity/degree/norm, depending on goal */
+    goal_matrix = (double *)calloc(n * n, sizeof(double));
+    if (!goal_matrix) {
+        fprintf(stderr, "An Error Has Occurred8\n");
         return 1;
     }
 
+    if (strcmp(goal, "sym") == 0) {
+        sym(data, n, d, goal_matrix);
+    } else if (strcmp(goal, "ddg") == 0) {
+        ddg(data, n, d, goal_matrix);
+    } else if (strcmp(goal, "norm") == 0) {
+        norm(data, n, d, goal_matrix);
+    } else {
+        fprintf(stderr, "An Error Has Occurred11\n");
+        free(goal_matrix);
+        free(data);
+        return 1;
+    }
+
+    print_matrix(goal_matrix, n, n); 
+    free(goal_matrix);
     free(data);
     return 0;
 }
